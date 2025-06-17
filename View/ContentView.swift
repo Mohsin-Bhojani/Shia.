6e6cb13efef8ac59a4c0d0173fc43d8dde972d4a
Mohsin <mohsin.husaynn@gmail.com>
2025-06-17 14:37:42 +0400

    Uodated the Content View

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -2,10 +2,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView {
-            SidebarView()
-        } detail: {
-            HomeView()
-        }
+        HomeView()
     }
 }
